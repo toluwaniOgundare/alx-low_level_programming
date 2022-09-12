a6f777383e358b32277d69fe8c6ca60ec8073eac
toluwaniOgundare <toluwani.ogundare@outlook.com>
2022-09-12 12:50:21 -0700

    script that uses the puts function to print a string in a c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,28 +1,14 @@
-#include <stdio.h>                                                                                                                                   
+#include <stdio.h>
 
-                                                                                                                                                     
-
-/**                                                                                                                                                  
-
-* main - print the string in the put function                                                                                                        
-
-*                                                                                                                                                    
-
-*Description: using the put function                                                                                                                 
-
-*this program prints "programming is like building a multilingual puzzle                                                                             
-
-*Return: 0                                                                                                                                           
-
-*/                                                                                                                                                   
-
-int main(void)                                                                                                                                       
-
-{                                                                                                                                                    
-
-        puts("\"Programming is like building a multilingual puzzle");                                                                                
-
-        return (0);                                                                                                                                   
-
-}  
-
+/**
+* main - print the string in the puts function
+*
+* Description: using the puts function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
